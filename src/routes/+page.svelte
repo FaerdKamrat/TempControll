48ceb44a63cd461574edc6f0b36b292f9c69f8dc
Oliver Bruhhult <oliverix21@gmail.com>
2026-04-22 14:28:04 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
 
     // --- CONFIG ---
-    const API_BASE = "http://stable-integral-searches-professionals.trycloudflare.com";
+    const API_BASE = "https://berlin-walls-banner-bloomberg.trycloudflare.com";
     const DISTANCE_STREAM_ID = "03c97b78-0167-446e-8bd0-cbc941561b65";
     const MATRIX_STREAM_ID = "e4904706-0687-4da7-9b05-9cbec06d689c";
     
